fa1589d64606b2c4479c027d9168bcea199ffbb7
ZLKCyber <88219725+Zlkcyber@users.noreply.github.com>
2024-11-14 13:31:06 +0800

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,12 @@
+DOMAIN_API = {
+    "SESSION": "https://api.nodepay.ai/api/auth/session",
+    "PING": [
+        "http://52.77.10.116/api/network/ping",
+        "http://13.215.134.222/api/network/ping",
+        "http://18.136.143.169/api/network/ping",
+        "http://52.74.35.173/api/network/ping",
+        "http://18.142.214.13/api/network/ping",
+        "http://18.142.29.174/api/network/ping",
+        "http://52.74.31.107/api/network/ping"
+    ]
+}
